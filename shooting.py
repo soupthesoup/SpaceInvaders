4d4ebfe1e9da909c2f50e9bd0c17dbe0a18a58bf
Dylan H <Dylanhism@users.noreply.github.com>
2017-02-14 09:38:45 -0500

    Update shooting.py

--- shooting.py
+++ shooting.py
@@ -6,4 +6,15 @@ import pygame.locals
 
 
 
-#invader location
+#Need invader location for instantiating
+WHITE = (255, 255, 255)
+done = False
+while not done:
+  for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+        #Check Surface name!!!
+        pygame.draw.rect(screen, WHITE, (position, 5, 10), 0)
+          #Rect(left, top, width, height)
+          #pygame.draw.rect(Surface, color, Rect, width=0) Width = line thickness 0 = full block
+      
