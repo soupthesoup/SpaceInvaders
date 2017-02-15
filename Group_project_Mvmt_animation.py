27398a23e4f7a36414d8a907c49b5e8823408080
MPL1999 <mplitt1999@gmail.com>
2017-02-15 09:35:27 -0500

    Update Group_project_Mvmt_animation.py

--- Group_project_Mvmt_animation.py
+++ Group_project_Mvmt_animation.py
@@ -2,6 +2,8 @@
 #               ICS4U1 movement and animations            #
 #                       Group poject                      #
 ###########################################################
+import pygame
+
 
 # movement for aliens function
 # def movment(
@@ -9,16 +11,20 @@
 
 
 # movement for ship
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        game = False
-        choose = True
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_q:
+def player_move(dsx, dx, dy):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             game = False
             choose = True
-        elif event.key == pygame.K_RIGHT:
-            dsx = 5
-        elif event.key == pygame.K_LEFT:
-            dsx = -5
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                game = False
+                choose = True
+            elif event.key == pygame.K_RIGHT:
+                dsx = 5
+            elif event.key == pygame.K_LEFT:
+                dsx = -5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                dsx = 0
         
