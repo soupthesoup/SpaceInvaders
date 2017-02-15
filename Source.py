8e1a76f3d1183ae492b966ec4350f28cdd41da28
MPL1999 <mplitt1999@gmail.com>
2017-02-15 10:01:44 -0500

    Update and rename Variables.py to Source.py

---
+++ Source.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+import random from randint
+pygame.init()
+screenSize (1000,800) 
+screen = pygame.display.set_mode((screenSize), 0)
+pygame.display.set_caption("Space Invaders")
+White(255,255,255)
+Green(0,255,0)
+Black(0,0,0)
+
+screen.fill(Black)
+pygame.display.update()
