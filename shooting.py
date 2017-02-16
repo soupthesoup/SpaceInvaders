1f52800b68ca6a04c973a445fb3d261883ca2e10
Dylan H <Dylanhism@users.noreply.github.com>
2017-02-16 09:19:01 -0500

    Update shooting.py

--- shooting.py
+++ shooting.py
@@ -11,29 +11,34 @@ def shoot():
   WHITE = (255, 255, 255)
   BLACK = (0, 0, 0)
   GREEN = (0, 204, 0) #Alien colour placeholder
-  dy = 1
+  bulletYspeed = -3
   #tank being the tank sprite.
-  position = tank.get_rect()
-  position.centerx = tank.get_rect().centerx
-  position.centery = tank.get_rect().centery - 10
+  bulPosition = tank.get_rect()
+  bulPosition.centerx = tank.get_rect().centerx
+  bulPosition.centery = tank.get_rect().centery - 10
+  
   #done = False
   #while not done:
   #  for event in pygame.event.get():
   #    if event.type == pygame.KEYDOWN:
   #      if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-
   #Check Surface name!!!
-  #Position +Animated ship position to be above it and in the middle
-  if position.y <= 800 and screen.get_at(position.x, position.y + 1) == (BLACK): #Change white to background colour
-    pygame.draw.rect(screen, WHITE, (position, 5, 10), 0)
-    position.y += dy
-    pygame.display.update()
-  elif screen.get_at(position.x, position.y + 1) != (BLACK):
-    pygame.draw.rect(screen, BLACK, (position, 5, 10), 0)
-    pygame.display.update()
-  elif screen.get_at(position.x, position.y + 1) == (GREEN):
-    #find alien position in the array somehow call it alienNum or whatnot.
-    del alienArray[alienNum]
-  #Rect(left, top, width, height)
-  #pygame.draw.rect(Surface, color, Rect, width=0) Width = line thickness 0 = full block
-      
+  #bulPosition +Animated ship bulPosition to be above it and in the middle
+  hit = False
+  while not hit:
+    if bulPosition.y > 0: #Change white to background colour
+      pygame.draw.rect(screen, WHITE, (bulPosition, 5, 10), 0)
+      bulPosition.top += dy
+      pygame.display.update()
+    elif bulPosition.top < 0:
+      pygame.draw.rect(screen, BLACK, (bulPosition, 5, 10), 0)
+      pygame.display.update()
+    elif pygame.colliderect(alien):
+      #pygame.sprite.collide_rect()
+      #find alien bulPosition in the array somehow call it alienNum or whatnot.
+      pygame.draw.rect(screen, BLACK, (bulPosition, 5, 10), 0)
+      del alienArray[alienNum]
+      hit = True
+    #Rect(left, top, width, height)
+    #pygame.draw.rect(Surface, color, Rect, width=0) Width = line thickness 0 = full block
+
