4e14a46f156bd6b32c0d81e620a21fb19a8e2262
MPL1999 <mplitt1999@gmail.com>
2017-02-27 09:20:22 -0500

    Update Sounds.py

--- Sounds.py
+++ Sounds.py
@@ -1,10 +1,11 @@
 import pygame
+# loads different sounds and puts them into variables to be called
 player_killed = pygame.mixer.music.load('explosion.wav')
 invader_killed = pygame.mixer.music.load('invaderkilled.wav')
 shoot = pygame.mixer.music.load('shoot.wav')
 
 
-
+# function for different sounds
 def player.explosion():
   pygame.mixer.Sound.play(player_killed)
   pygame.mixer.music.stop()
