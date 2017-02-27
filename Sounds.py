e1158c485c20d9523f0788f80d4447512e8db800
MPL1999 <mplitt1999@gmail.com>
2017-02-27 09:09:36 -0500

    Update Sounds.py

--- Sounds.py
+++ Sounds.py
@@ -1,3 +1,19 @@
 import pygame
+player_killed = pygame.mixer.music.load('explosion.wav')
+invader_killed = pygame.mixer.music.load('invaderkilled.wav')
+shoot = pygame.mixer.music.load('shoot.wav')
 
-def player.explosion()
+
+
+def player.explosion():
+  pygame.mixer.Sound.play(player_killed)
+  pygame.mixer.music.stop()
+
+def invader.killed():
+	pygame.mixer.Sound.play(invader_killed)
+	pygame.mixer.music.stop()
+
+def shoot():
+	pygame.mixer.Sound.play(shoot)
+	pygame.mixer.music.stop()
+  
