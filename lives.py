f759c186bf11f20441fae50f935857e93a7acd06
MPL1999 <mplitt1999@gmail.com>
2017-02-28 09:43:30 -0500

    Update lives.py

--- lives.py
+++ lives.py
@@ -1,8 +1,19 @@
+lifeCount = 3
+initialLives = [
+    "LLL"
+    ]
 for row in initialLives:
     for col in row:
         if col == "L":
             lifeRect = pygame.Rect(lx,ly,20,20)
             Lives.append(lifeRect)
         lx += 25
+    for alienBullet in alienBulletArray:
+        if alienBullet.colliderect(shipRect):
+            initialLives.
         
 for life in initialLives:
+    pygame.draw.rect(screen, White, life, 0)
+    life.left += 50
+    
+    
